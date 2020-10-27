648d483e890067836c5aa4dd8bd74dd089b52350
Sean Oyler <xrysen@gmail.com>
2020-10-27 20:43:41 +0000

    Added without.js

---
+++ without.js
@@ -0,0 +1,45 @@
+const eqArrays = function (arr1, arr2) {
+  let n = 0;
+  while (n < arr1.length) {
+    if (arr1[n] !== arr2[n])
+      return false;
+    else
+      n++;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`😁 😁 😁 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🤨 🤨 🤨 Assertion Failed! ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const without = (source, itemsToRemove) => {
+  let returnArray = source;
+
+  for (let i = 0; i < source.length; i++) {
+    if (Array.isArray(itemsToRemove)) {
+      for (let j = 0; j < itemsToRemove.length; j++) {
+        if (source[i] === itemsToRemove[j])
+          returnArray.splice(i, 1);
+      }
+    } else {
+      if(source[i] === itemsToRemove) {
+        returnArray.splice(i,1);
+      }
+    }
+  }
+
+  return returnArray;
+};
+
+assertArraysEqual(without([1, 2, "Hi", 3, 4, 5, 10, 14, "Hi"], 5), [1,2, "Hi", 3, 4, 10, 14, "Hi"]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
