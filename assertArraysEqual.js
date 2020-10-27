9681385ac3ba39cef4abdcec7e68abb35342c6cf
Sean Oyler <xrysen@gmail.com>
2020-10-27 19:19:16 +0000

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(arr1, arr2) {
+  let n = 0;
+  while (n < arr1.length) {
+    if (arr1[n] !== arr2[n])
+      return false;
+    else
+      n++;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`😁 😁 😁 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🤨 🤨 🤨 Assertion Failed! ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual(["cat","dog"], [1, "mouse"]);
+assertArraysEqual([1.2, 3.1], [1.2, 3.1]);
