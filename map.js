a1be6a11400cb46b0341602b0d79a92d5f74a922
Sean Oyler <xrysen@gmail.com>
2020-10-29 20:18:19 +0000

    Added map.js

---
+++ map.js
@@ -0,0 +1,40 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const eqArrays = function (arr1, arr2) {
+  let n = 0;
+  if (arr1.length !== arr2.length)
+    return false;
+  else {
+    while (n < arr1.length) {
+      if (arr1[n] !== arr2[n])
+        return false;
+      else
+        n++;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`😁 😁 😁 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🤨 🤨 🤨 Assertion Failed! ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word + "s");
+
+assertArraysEqual(results1, ['g','c','t','m','t']);
+assertArraysEqual(results2, ['grounds','controls','tos','majors','toms']);
