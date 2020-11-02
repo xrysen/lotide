ca1cbaf2f30047b8299ea61ea41c4c48907030c4
Sean Oyler <xrysen@gmail.com>
2020-11-02 05:15:10 +0000

    Added ability to check multi-nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😁 😁 😁 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,22 +6,31 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (arr1, arr2) {
-  let n = 0;
+const eqArrays = function(arr1, arr2) {
+
   if (arr1.length !== arr2.length)
     return false;
-  else {
-    while (n < arr1.length) {
-      if (arr1[n] !== arr2[n])
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (eqArrays(arr1[i], arr2[i]) && eqArrays(arr1[i + 1], arr2[i + 1])) {
+        return true;
+      } else {
         return false;
-      else
-        n++;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
     }
   }
   return true;
 };
 
+
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays(["A", "B", "C"], ["a", "B", "C"]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([3, 1], [3, 1]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4], [[4,2]]], [[2, 3], [4], [[4,2]]]), true);
