6183a8458d943f21b9a2744b2a558315d36ef275
Sean Oyler <xrysen@gmail.com>
2020-11-03 04:01:42 +0000

    Refactored eqArrays to use Mocha tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,20 @@
 const eqArrays = require("../eqArrays");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["A", "B", "C"], ["a", "B", "C"]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([3, 1], [3, 1]), true);
+describe("#eqArrays", () => {
+  it("should return true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4], [[4,2]]], [[2, 3], [4], [[4,2]]]), true);
+  it("should return false for ['A', 'B', 'C'], ['a', 'B', 'C']", () => {
+    assert.deepEqual(eqArrays(["A", "B", "C"], ["a", "B", "C"]), false);
+  });
+
+  it("should return false for ['1', '2', '3'], ['1', '2', 3])", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  it("should return true for [[2, 3], [4], [[4,2]]], [[2, 3], [4], [[4,2]]]", () => {
+    assert.deepEqual(eqArrays([[2, 3], [4], [[4, 2]]], [[2, 3], [4], [[4, 2]]]), true);
+  });
+});
